5b54531ce5b413f2f71e03ba5d16f007ab6bcbf0
KevinGCodes <91242602+KevinGCodes@users.noreply.github.com>
2021-12-29 21:38:31 +0100

    changed Layout to Grouplayout

--- src/src/View/MainFrame.java
+++ src/src/View/MainFrame.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     Subject dataset;
 
-    public MainFrame(Subject dataset){
+    public MainFrame(Subject dataset, DataPlot dataplot){
         super("Clustering");
         this.dataset = dataset;
 
@@ -16,7 +16,19 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(new Dimension(800, 800));
         this.setBackground(Color.WHITE);
-        this.add(new DataPlot(this.dataset, this));
+        var settingsPanel = new SettingsPanel(this.dataset);
+
+        var contentPane = this.getContentPane();
+        contentPane.setBackground(Color.black);
+        var layout = new GroupLayout(contentPane);
+        this.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        layout.setHorizontalGroup(layout.createSequentialGroup().addComponent(dataplot)
+                                                                .addComponent(settingsPanel));
+        layout.setVerticalGroup(layout.createParallelGroup().addComponent(dataplot)
+                                                            .addComponent(settingsPanel));
 
     }
 }
