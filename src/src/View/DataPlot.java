77957119598aa633d6392e6d02c9f4d68408a3c6
KevinGCodes <91242602+KevinGCodes@users.noreply.github.com>
2021-12-29 21:39:21 +0100

    added a coordinateSystem to the gui

--- src/src/View/DataPlot.java
+++ src/src/View/DataPlot.java
@@ -9,22 +9,26 @@ import java.util.List;
 
 public class DataPlot extends JPanel implements Observer, MouseListener {
     private Subject dataset;
-    private JFrame frame;
     private final int WIDTH = 600;
     private final int HEIGHT = 600;
+    private final int PADDING = 75;
+    private Graphics graphic;
 
-    public DataPlot(Subject dataset, JFrame frame){
-        this.frame = frame;
+    public DataPlot(Subject dataset){
         this.dataset = dataset;
-        setSize(new Dimension(400, 400));
+        setMinimumSize(new Dimension(400, 400));
+        setPreferredSize(new Dimension(600, 600));
+        setMaximumSize(new Dimension(800, 800));
         this.addMouseListener(this);
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        List<Point> data = dataset.getData();
+        graphic = g;
         setBackground(Color.darkGray);
+        drawCoordinateSystem(g);
+        List<Point> data = dataset.getData();
 
         for(Point p : data){
             int px = (int)Math.round(p.getX()*getWidth());
@@ -36,10 +40,22 @@ public class DataPlot extends JPanel implements Observer, MouseListener {
 
     }
 
-    @Override
-    public void updateObserver() {
-        this.repaint();
+    private void drawCoordinateSystem(Graphics g){
+        int lineWidth = getWidth()- 2*PADDING;
+        int lineHeight = getHeight() - 2*PADDING;
+        g.setColor(Color.WHITE);
+        g.drawLine(PADDING, PADDING, PADDING, PADDING + lineHeight);
+        g.drawLine(PADDING, PADDING + lineHeight, PADDING + lineWidth, PADDING + lineHeight);
+        final float stepSize = 0.2f;
+        g.drawString("0", PADDING - 5, PADDING + lineHeight + 12);
+        for(int i = 1; i <= 5; i++){
+            long round = Math.round(PADDING + i * 0.2 * lineWidth);
+            g.drawString(Float.toString(i*0.2f),(int)round - 7, PADDING + lineHeight + 12);
+            g.drawString(Float.toString(i*0.2f),PADDING - 20,(int)Math.round(PADDING + lineHeight - i * 0.2 * lineHeight));
+        }
+
     }
+
 
     @Override
     public void mouseClicked(MouseEvent e) {}
@@ -48,8 +64,8 @@ public class DataPlot extends JPanel implements Observer, MouseListener {
     public void mousePressed(MouseEvent e) {
         System.out.println("mouse-coordinates: " + e.getX() + " + " + e.getY());
         System.out.println(dataset.getData());
+        if(e.getX() < PADDING || e.getY() < PADDING) return;
         dataset.addPoint((float)e.getX()/getWidth(),(float)e.getY()/getHeight());
-        this.updateObserver();
     }
 
     @Override
@@ -60,4 +76,9 @@ public class DataPlot extends JPanel implements Observer, MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void updateObserver() {
+        repaint(0);
+    }
 }
