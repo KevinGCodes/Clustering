72d29313c0ded07d6a56d2e3efb387b905f911ff
KevinGCodes <91242602+KevinGCodes@users.noreply.github.com>
2021-12-30 19:25:28 +0100

    factored common code of KMeans and PAM out into the common interface

--- src/src/Algorithms/ClusteringAlgorithm.java
+++ src/src/Algorithms/ClusteringAlgorithm.java
@@ -2,9 +2,67 @@ package Algorithms;
 
 import Model.Point;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public interface ClusteringAlgorithm {
-    Map<Point, List<Point>> cluster();
+    Map<Point, List<Point>> cluster(int k);
+    List<Point> getPoints();
+
+    static double calculateVariance(Map<Point, List<Point>> clustering){
+        double variance = 0;
+        variance = clustering.values().stream().mapToDouble(arr -> {
+            Point mean = calculateMean(arr);
+            return arr.stream().mapToDouble(p -> (p.getDiff(mean)*p.getDiff(mean))).sum();
+        }).sum();
+
+        return variance;
+    }
+
+    static Point calculateMean(List<Point> points){
+        if(points == null || points.size() == 0) return new Point(0, 0);
+        double sumX = points.stream().mapToDouble(p -> p.getX()).sum();
+        double sumY = points.stream().mapToDouble(p -> p.getY()).sum();
+        return new Point(sumX/points.size(), sumY/points.size());
+    }
+
+    default Map<Point, List<Point>> cluster(){
+        int len = getPoints().size();
+        double[] variances = new double[len + 1];
+        for(int k = 1; k <= len; k++){
+            var clustering = cluster(k);
+            variances[k] = ClusteringAlgorithm.calculateVariance(clustering);
+        }
+        int bestK = ElbowPointCalculator.findElbow(variances);
+        return cluster(bestK);
+    }
+
+    default List<Point> getReferencePoints(int k){
+        ArrayList<Point> referencePoints = new ArrayList<Point>();
+        Random rand = new Random();
+
+        for(int i = 0; i < k; i++){
+            int index = rand.nextInt(getPoints().size());
+            while(referencePoints.contains(getPoints().get(index))) index = rand.nextInt(getPoints().size());
+            referencePoints.add(getPoints().get(index));
+        }
+        return referencePoints;
+    }
+
+    default HashMap<Point, List<Point>> Iteration(List<Point> referencePoints){
+        int k = referencePoints.size();
+        HashMap<Point, List<Point>> clustering = new HashMap<>();
+        for(Point p : referencePoints) clustering.put(p, new ArrayList<>());
+        for(Point p : getPoints()){
+            double min = Double.MAX_VALUE;
+            for(Point rp : referencePoints){
+                double diff = p.getDiff(rp);
+                if(diff < min){
+                    min = diff;
+                    for(List<Point> arr : clustering.values()) arr.remove(p);
+                    clustering.get(rp).add(p);
+                }
+            }
+        }
+        return clustering;
+    }
 }
--- src/src/Algorithms/KMeansClustering.java
+++ src/src/Algorithms/KMeansClustering.java
@@ -9,50 +9,31 @@ public class KMeansClustering implements ClusteringAlgorithm {
     public KMeansClustering(List<Point> dataPoints){
         this.points = dataPoints;
     }
+
     @Override
-    public Map<Point, List<Point>> cluster(){
-        int len = Math.min(10, points.size());
-        double[] variances = new double[len + 1];
-        for(int k = 1; k <= len; k++){
-            var clustering = cluster(k);
-            variances[k] = calculateVariance(clustering);
-        }
-        for(double f : variances) System.out.print("   " + f);
-        int bestK = 1;
-        double maxDiff = 0;
-        for(int k = 2; k < len; k++){
-            double v1 = variances[k - 1] - variances[k];
-            double v = v1;
-            if(v > maxDiff){
-                bestK = k;
-                maxDiff = v;
-            }
-        }
-        System.out.println(bestK);
-        return cluster(bestK);
-    }
     public Map<Point, List<Point>> cluster(int k){
-        k = Math.min(k, points.size());
+        System.out.println("neeiiiin");
+        k = Math.min(k, getPoints().size());
         List<Point> referencePoints = getReferencePoints(k);
         HashMap<Point, List<Point>> clustering = Iteration(referencePoints);
-        for(int i = 0; i < 50; i++){
+        for(int i = 0; i < 15; i++){
             List<Point> tmp = new ArrayList<>();
-            clustering.values().forEach(arr -> tmp.add(calculateMean(arr)));
+            clustering.values().forEach(arr -> tmp.add(ClusteringAlgorithm.calculateMean(arr)));
             referencePoints = tmp;
             clustering = Iteration(referencePoints);
         }
-        double minVariance = calculateVariance(clustering);
+        double minVariance = ClusteringAlgorithm.calculateVariance(clustering);
         HashMap<Point, List<Point>> bestClustering = clustering;
-        for(int i = 0; i< 200; i++){
+        for(int i = 0; i< 100; i++){
             List<Point> newReferencePoints = getReferencePoints(k);
             HashMap<Point, List<Point>> newClustering = Iteration(newReferencePoints);
-            for(int j = 0; j < 50; j++){
+            for(int j = 0; j < 15; j++){
                 List<Point> newTmp = new ArrayList<>();
-                newClustering.values().forEach(arr -> newTmp.add(calculateMean(arr)));
+                newClustering.values().forEach(arr -> newTmp.add(ClusteringAlgorithm.calculateMean(arr)));
                 newReferencePoints = newTmp;
                 newClustering = Iteration(newReferencePoints);
             }
-            double newVariance = calculateVariance(newClustering);
+            double newVariance = ClusteringAlgorithm.calculateVariance(newClustering);
             if(newVariance < minVariance){
                 minVariance = newVariance;
                 bestClustering = newClustering;
@@ -61,50 +42,9 @@ public class KMeansClustering implements ClusteringAlgorithm {
         return bestClustering;
     }
 
-    public List<Point> getReferencePoints(int k){
-        ArrayList<Point> referencePoints = new ArrayList<Point>();
-        Random rand = new Random();
-
-        for(int i = 0; i < k; i++){
-            int index = rand.nextInt(points.size());
-            while(referencePoints.contains(points.get(index))) index = rand.nextInt(points.size());
-            referencePoints.add(points.get(index));
-        }
-        return referencePoints;
+    @Override
+    public List<Point> getPoints() {
+        return this.points;
     }
 
-    public static double calculateVariance(Map<Point, List<Point>> clustering){
-        double variance = 0;
-        variance = clustering.values().stream().mapToDouble(arr -> {
-            Point mean = calculateMean(arr);
-            return arr.stream().mapToDouble(p -> (p.getDiff(mean)*p.getDiff(mean))).sum();
-        }).sum();
-
-        return variance;
-    }
-
-    public static Point calculateMean(List<Point> points){
-        if(points == null || points.size() == 0) return new Point(0, 0);
-        double sumX = points.stream().mapToDouble(p -> (double)p.getX()).sum();
-        double sumY = points.stream().mapToDouble(p -> (double)p.getY()).sum();
-        return new Point((float)sumX/points.size(), (float)sumY/points.size());
-    }
-
-    public HashMap<Point, List<Point>> Iteration(List<Point> referencePoints){
-        int k = referencePoints.size();
-        HashMap<Point, List<Point>> clustering = new HashMap<>();
-        for(Point p : referencePoints) clustering.put(p, new ArrayList<Point>());
-        for(Point p : points){
-            double min = Double.MAX_VALUE;
-            for(Point rp : referencePoints){
-                double diff = p.getDiff(rp);
-                if(diff < min){
-                    min = diff;
-                    for(List<Point> arr : clustering.values()) arr.remove(p);
-                    clustering.get(rp).add(p);
-                }
-            }
-        }
-        return clustering;
-    }
 }
